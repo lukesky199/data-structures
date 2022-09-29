f7ca06263e96bf9cf4c830ef0206f1ae682eb268
Lukesky199 <lbli2@illinois.edu>
2022-09-29 21:12:10 +0000

    Completed find() and insert()

--- lab_bst/src/bst.hpp
+++ lab_bst/src/bst.hpp
@@ -7,51 +7,86 @@
 template <class K, class V>
 V BST<K, V>::find(const K& key)
 {
-    // your code here
+    // Your code here
+    if (find(root, key) == nullptr) {
+        return V();
+    }
+    return find(root, key)->value;
 }
 
 template <class K, class V>
 struct BST<K, V>::Node* & BST<K, V>::find(Node* & subtree, const K& key)
 {
     // Your code here
+    if (subtree == nullptr) {
+        return subtree;
+    }
+    if (subtree->key == key) {
+        return subtree;
+    }
+    if (subtree->key > key) {
+        return find(subtree->left, key);
+    }
+    else {
+        return find(subtree->right, key);
+    }
 }
 
 template <class K, class V>
 void BST<K, V>::insert(const K & key, const V & value)
 {
     // your code here
+    insert(root, key, value);
 }
 
 template <class K, class V>
 void BST<K, V>::insert(Node*& subtree, const K& key, const V& value)
 {
     // your code here
+    if (subtree == nullptr) {
+        subtree = new Node(key, value);
+        return;
+    }
+    if (subtree->key == key) {
+        return;
+    }
+    if (subtree->key > key) {
+        insert(subtree->left, key, value);
+    }
+    else {
+        insert(subtree->right, key, value);
+    }
 }
 
 template <class K, class V>
 void BST<K, V>::swap(Node*& first, Node*& second)
 {
     // your code here
+    assert(false);
 }
 
 template <class K, class V>
 void BST<K, V>::remove(const K& key)
 {
     // your code here
+    assert(false);
 }
 
 template <class K, class V>
 void BST<K, V>::remove(Node*& subtree, const K& key)
 {
     // your code here
+    assert(false);
 }
 
 template <class K, class V>
 BST<K, V> listBuild(std::vector<std::pair<K, V>> inList){
     // your code here
+    assert(false);
 }
 
 template <class K, class V>
 std::vector<int> allBuild(std::vector<std::pair<K, V>> inList){
     // your code here
+    assert(false);
 }
