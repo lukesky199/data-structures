60a08aa551020ca00d068a8c88989e22813dd762
Lukesky199 <lbli2@illinois.edu>
2022-10-02 21:37:14 +0000

    Completed lab_bst

--- lab_bst/src/bst.hpp
+++ lab_bst/src/bst.hpp
@@ -62,31 +62,92 @@ template <class K, class V>
 void BST<K, V>::swap(Node*& first, Node*& second)
 {
     // your code here
-    assert(false);
+    if (first == nullptr || second == nullptr) {
+        std::cout << "BST<K, V>::swap(Node*& first, Node*& second) : first or second is nullptr" << std::endl;
+        return;
+    }
+    std::swap(first->key, second->key);
+    std::swap(first->value, second->value);
 }
 
 template <class K, class V>
 void BST<K, V>::remove(const K& key)
 {
     // your code here
-    assert(false);
+    remove(root, key);
 }
 
 template <class K, class V>
 void BST<K, V>::remove(Node*& subtree, const K& key)
 {
     // your code here
-    assert(false);
+    auto& delNode = find(subtree, key);
+    if (delNode == nullptr) {
+        return;
+    }
+
+    if (delNode->left == nullptr && delNode->right == nullptr) {
+        delete delNode;
+        delNode = nullptr;
+        return;
+    }
+
+    if (delNode->right == nullptr) { //Left one child
+        auto temp = delNode;
+        delNode = delNode->left;
+        delete temp;
+        return;
+    }
+
+    if (delNode->left == nullptr) { //Right one child
+        auto temp = delNode;
+        delNode = delNode->right;
+        delete temp;
+        return;
+    }
+
+    // Two children
+    auto current = delNode->left;
+    auto parent = delNode;
+    while (current->right != nullptr) {
+        parent = current;
+        current = current->right;
+    }
+    swap(delNode, current);
+    if (current->left != nullptr) {
+        if (parent == delNode) {
+            parent->left = current->left;
+        } else {
+            parent->right = current->left;
+        }
+    } else {
+        parent->right = nullptr;
+    }
+    delete current;
+
 }
 
 template <class K, class V>
 BST<K, V> listBuild(std::vector<std::pair<K, V>> inList){
     // your code here
-    assert(false);
+    BST<K, V> ret;
+    for (auto node : inList) {
+        ret.insert(node.first, node.second);
+    }
+    return ret;
 }
 
 template <class K, class V>
 std::vector<int> allBuild(std::vector<std::pair<K, V>> inList){
     // your code here
-    assert(false);
+    std::vector<int> heights;
+    std::sort(inList.begin(), inList.end());
+    do {
+        unsigned height = listBuild(inList).height();
+        if (height >= heights.size()) {
+            heights.resize(height + 1, 0);
+        }
+        heights[height]++;
+    } while (std::next_permutation(inList.begin(), inList.end()));
+    return heights;
 }
