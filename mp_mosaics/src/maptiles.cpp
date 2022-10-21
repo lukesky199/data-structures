2a40ff410999febebcdb0840e2f6103c8facde0a
Lukesky199 <lbli2@illinois.edu>
2022-10-21 22:08:13 +0000

    Passed mp_mosaics part 2 tests

--- mp_mosaics/src/maptiles.cpp
+++ mp_mosaics/src/maptiles.cpp
@@ -21,7 +21,34 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     /**
      * @todo Implement this function!
      */
+    MosaicCanvas* ret = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
 
-    return NULL;
+    //Map <AverageColorPoint, TileImage>
+    std::map<Point<3>, TileImage*> tileMap;
+    std::vector<Point<3>> points;
+
+    // Initialize tileMap and points;
+    for (auto& tile : theTiles) {
+        LUVAPixel avgColor = tile.getAverageColor();
+        Point<3> avgPoint(avgColor.l, avgColor.u, avgColor.v);
+        tileMap[avgPoint] = &tile;
+        points.push_back(Point<3>(avgColor.l, avgColor.u, avgColor.v));
+    }
+
+    //kD-Tree of LUVPixel points
+    KDTree<3> tileTree(points);
+
+    std::cout << "COLUMNS: " << ret->getColumns() << std::endl;
+    std::cout << "ROWS: " << ret->getRows() << std::endl;
+    // Make Tiles
+    for (int i = 0; i < ret->getRows(); i++) {
+        for (int j = 0; j < ret->getColumns(); j++) {
+            LUVAPixel color = theSource.getRegionColor(i, j);
+            Point<3> point = tileTree.findNearestNeighbor(Point<3>(color.l, color.u, color.v));
+            ret->setTile(i, j, tileMap[point]);
+        }
+    }
+
+    return ret;
 }
 
